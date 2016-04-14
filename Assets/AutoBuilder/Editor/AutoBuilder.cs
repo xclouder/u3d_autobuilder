2c98794c02ff74451eb4a8208245b031a33f02b1
xclouder <xclouder@qq.com>
2016-04-14 22:45:52 +0800

    android build path fix

--- Assets/AutoBuilder/Editor/AutoBuilder.cs
+++ Assets/AutoBuilder/Editor/AutoBuilder.cs
@@ -9,33 +9,28 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Linq;
 
 public class AutoBuilder
 {
 
 	[MenuItem("Build/* Start Build Package",false,1019)]
 	public static void BuildPackage(){
-		if(EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android){
-
-			CIBuild.BuildPackage (BuildTarget.Android,BuildOptions.AcceptExternalModificationsToPlayer);
-		}else{
-			CIBuild.BuildPackage (EditorUserBuildSettings.activeBuildTarget);
-		}
+		CIBuild.BuildPackage (EditorUserBuildSettings.activeBuildTarget);
 	}
 
-
-	[MenuItem("Build/Build For Platform/Android", false, 1020)]
+	[MenuItem("Build/Build for Platform/Android", false, 1020)]
 	public static void BuildAndroidPackage(){
-		CIBuild.BuildPackage (BuildTarget.Android,BuildOptions.AcceptExternalModificationsToPlayer);
+		CIBuild.BuildPackage (BuildTarget.Android);
 	}
 
-	[MenuItem("Build/Build For Platform/Android", true, 1020)]
+	[MenuItem("Build/Build for Platform/Android", true, 1020)]
 	static bool ValidateBuildAndroidPackage()
 	{
 		return EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android;
 	}
 
-	[MenuItem("Build/Build For Platform/iOS",false,1020)]
+	[MenuItem("Build/Build for Platform/iOS",false,1020)]
 	public static void BuildiOSPackage(){
 		#if UNITY_5
 		CIBuild.BuildPackage (BuildTarget.iOS);
@@ -44,7 +39,7 @@ public class AutoBuilder
 		#endif
 	}
 
-	[MenuItem("Build/Build For Platform/iOS", true, 1020)]
+	[MenuItem("Build/Build for Platform/iOS", true, 1020)]
 	static bool ValidateBuildiOSPackage()
 	{
 		#if UNITY_5
@@ -60,39 +55,5 @@ public class AutoBuilder
 	{
 		AndroidBuildSettings.Edit ();
 	}
-
-	/*
-	[MenuItem("JDKLib/Debug/Build Android Native Jar",false,2038)]
-	public static void BuildJar()
-	{
-		AndroidNativeJarBuildScript.BuildJar ();
-	}
-
-	[MenuItem("JDKLib/PreBuild Project",false,1020)]
-	public static void Prebuild(){
-		JDKPreBuildProcessor.PreBuild ();
-	}
-
-	[MenuItem("JDKLib/Module Settings",false,1021)]
-	public static void ModuleSettings()
-	{
-		JDKModuleSettings.Edit ();
-	}
-
-	[MenuItem("JDKLib/Debug/Export Eclipse Project",false,2046)]
-	public static void BuildAndroidProject(){
-		CIBuild.BuildPackage (BuildTarget.Android,BuildOptions.AcceptExternalModificationsToPlayer);
-	}
-
-	[MenuItem("JDKLib/Debug/Test CI Build iOS",false,2046)]
-	public static void TestAutoBuildiOSProject(){
-		CIBuild.CommandLineBuildiOS();
-	}
-
-	[MenuItem("JDKLib/Debug/Test CI Build Android",false,2046)]
-	public static void TestAutoBuildAndroidProject(){
-		CIBuild.CommandLineBuildAndroid();
-	}
-	*/
 
 }
--- Assets/AutoBuilder/Editor/CIBuild.cs
+++ Assets/AutoBuilder/Editor/CIBuild.cs
@@ -16,8 +16,9 @@ public static class CIBuild{
 
 	#region private
     private static string GetiOSBuildPath(){
-        string dirPath = Application.dataPath + "/../../../build/iPhone";
-        Debug.LogWarning("Build Dir:" + dirPath);
+		string dirPath = Application.dataPath.TrimEnd("Assets");
+		dirPath += "builds";
+
         if (!Directory.Exists(dirPath)){
             Directory.CreateDirectory(dirPath);
         }
@@ -25,7 +26,9 @@ public static class CIBuild{
     }
 
 	private static string GetAndroidBuildPath(){
-		string dirPath = Application.dataPath + "/../../../build/android";
+		string dirPath = Application.dataPath.TrimEnd("Assets");
+		dirPath += "builds";
+
 		if (!Directory.Exists(dirPath)){
 			Directory.CreateDirectory(dirPath);
 		}
@@ -43,28 +46,15 @@ public static class CIBuild{
 	}
 
 	#endregion
-	public static void CommandLIneBuild()
-	{
-		BuildTarget target = BuildTarget.Android;
+
+    public static void CommandLineBuildiOS(){
+		BuildTarget target = BuildTarget.iOS;
 
 		if (EditorUserBuildSettings.activeBuildTarget != target){
 			EditorUserBuildSettings.SwitchActiveBuildTarget(target);
 		}
 
-		string[] scenes = GetBuildScenes();
-		string path = GetAndroidBuildPath();
-		if (scenes == null || scenes.Length == 0 || path == null)
-		{
-			Debug.LogError("Scene is empty, exit");
-			return;
-		}
-
-
-		
-	}
-
-    public static void CommandLineBuildiOS(){
-		throw new System.NotImplementedException();
+		DoBuildPackage(BuildTarget.iOS, GetiOSBuildPath());
     }
 
     public static void CommandLineBuildAndroid(){
@@ -74,39 +64,38 @@ public static class CIBuild{
 			EditorUserBuildSettings.SwitchActiveBuildTarget(target);
 		}
 
-        string[] scenes = GetBuildScenes();
-        string path = GetAndroidBuildPath();
-        if (scenes == null || scenes.Length == 0 || path == null)
-            return;
-
-//        Debug.Log(string.Format("Path: \"{0}\"", path));
-//        for (int i = 0; i < scenes.Length; ++i){
-//            Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
-//        }
-
-		var bundleId = "r2games.bundle.test";
-		var shortVer = "1.0";
-		var productName = "productName";
-
-        Debug.Log("Starting Android Build!");
-		string[] bundleIdSplit = bundleId.Split('.');
-		string packageName = bundleIdSplit[bundleIdSplit.Count()-1];
-		string buildPath = path+"/"+packageName+ "_" + shortVer;
-        string finalPath = buildPath + "_" + System.DateTime.Now.ToString("yyMMddHHmm") + ".apk";
-        BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.Android, BuildOptions.AcceptExternalModificationsToPlayer);
-
-        BuildAPKPackage(buildPath,false,finalPath);
+		DoBuildPackage(BuildTarget.Android, GetAndroidBuildPath());
     }
 
+	public static void DoBuildPackage(BuildTarget target, string path, BuildOptions option = BuildOptions.None)
+	{
+		//TODO:pre build
+		Debug.Log("Prebuilding...");
+		//....
+
+		Debug.Log("Start BuildPlayer...");
+		BuildPipeline.BuildPlayer(GetBuildScenes(), path, target, option);
+
+		Debug.Log("Build completed");
+	}
+
     public static void BuildPackage(BuildTarget target, BuildOptions option = BuildOptions.None){
-		var bundleId = "r2games.bundle.test";
-		var shortVer = "1.0";
-		var productName = "productName";
+
+		//init properties
+		#if UNITY_IPHONE
+		var bundleId = PlayerSettings.iPhoneBundleIdentifier;
+		#else
+		var bundleId = PlayerSettings.bundleIdentifier;
+		#endif
+
+		var shortVer = PlayerSettings.bundleVersion;
+		var productName = PlayerSettings.productName;
 
         if (EditorApplication.isCompiling){
             EditorUtility.DisplayDialog("错误提示", "Unity Editor 正在编译代码,请编译完成后再构建.", "取消");
             return;
         } 
+
         if (EditorUserBuildSettings.activeBuildTarget != target){
             if (EditorUtility.DisplayDialog("目标平台切换", "需要先切换到:" + target.ToString() + " 平台才能继续构建,是否立即切换?", "立即切换", "取消构建")){
                 Debug.LogWarning("SwitchActiveBuildTarget to " + target.ToString());
@@ -119,11 +108,13 @@ public static class CIBuild{
                 return;
             }
         }
-//        if (JDKPlayerSettingsEditor.BuildSettingsAlert(true)){
+
+//        if (AutoBuilderSettings.BuildSettingsAlert(true)){
 		if (true){
             string fileName;
             string ext;
 			bool isDebugMode = false;
+
             if (target == BuildTarget.Android){
                 if(!isDebugMode)
                     PlayerSettings.Android.bundleVersionCode += 1;
@@ -138,7 +129,7 @@ public static class CIBuild{
                 fileName =productName + "_" + shortVer;
                 ext = "";
             }
-            var path = EditorUtility.SaveFilePanel("保存到(选择已经存在的路劲会覆盖原有的包,文件名:" + fileName + ")", PrePath == "" ? "" : Path.GetDirectoryName(PrePath), fileName, ext);
+            var path = EditorUtility.SaveFilePanel("保存到(选择已经存在的路劲会覆盖原有的包)", PrePath == "" ? "" : Path.GetDirectoryName(PrePath), fileName, ext);
             if (path.Length != 0){
                 PrePath = path;
 
@@ -148,18 +139,8 @@ public static class CIBuild{
                     return;
                 }
 
-				//TODO:pre build
-//                JDK.EditorUtils.JDKPreBuildProcessor.PreBuild();
-                Debug.LogWarning("Start BuildPlayer.");
-                BuildPipeline.BuildPlayer(GetBuildScenes(), path, target, option);
-                //调用项目自定义钩子
-                string buildedPath = target == BuildTarget.Android ? Path.Combine(path, productName) : path;
-                
-                if(BuildOptions.AcceptExternalModificationsToPlayer == option && target == BuildTarget.Android){
-                    BuildAPKPackage(path,true);
-                }else{
-                    OpenInFileBrowser.Open(path);
-                }
+				DoBuildPackage(target, path, option);
+
             } else{
                 Debug.LogWarning("Cancel build.");
             }
@@ -168,72 +149,4 @@ public static class CIBuild{
         }
     }
 
-    private static void BuildAPKPackage(string path,bool isOpenFolderOnFinish,string finishAPKPath=""){
-		var bundleId = "r2games.bundle.test";
-		var shortVer = "1.0";
-		var productName = "productName";
-
-        //使用gradle构建apk包
-        //Unity导出android工程会自动加项目名作为最终目录，把它移出来
-        var info = new DirectoryInfo (Path.Combine(path, productName));
-        foreach (FileSystemInfo fsi in info.GetFileSystemInfos()) {
-            string destName = Path.Combine (path, fsi.Name);
-            if(File.Exists(destName)||Directory.Exists(destName)){
-                Debug.LogWarning("remove old :"+destName);
-                FileLinkUtil.ForceRemove(destName);
-            }
-            if (fsi is FileInfo) {
-                Debug.Log("move :"+fsi.FullName+" to :"+destName);
-                File.Move(fsi.FullName, destName) ;
-            }else{
-                Debug.Log("move :"+fsi.FullName+" to :"+destName);
-                Directory.Move(fsi.FullName,destName) ;
-            }
-        }
-        Directory.Delete(Path.Combine(path, productName));
-        File.Copy(Application.dataPath + "/JDKLib/Editor/Tools/apk_build.gradle",
-            Path.Combine(path, "build.gradle"),true);
-        File.Copy(Path.Combine (Application.dataPath, "JDKLib/Editor/Android/ext/releasesig.keystore"),
-            Path.Combine(path, "releasesig.keystore"),true);
-        Debug.Log ("===========start APK 构建==============");
-        EditorUtility.DisplayProgressBar(
-            "正在构建APK包",
-            "请等待1-2分钟,首次构建时间可能需要5+分钟",
-            0.8f);
-        var proc = new System.Diagnostics.Process();
-        proc.StartInfo.UseShellExecute = false;
-        proc.StartInfo.CreateNoWindow = true;
-        
-        proc.StartInfo.RedirectStandardError = true;
-        proc.EnableRaisingEvents=false; 
-        proc.StartInfo.FileName = Application.dataPath + "/JDKLib/Editor/Tools/apk_build.sh";
-        proc.StartInfo.Arguments = "\""+path+"\"";
-        proc.Start();
-        var err = proc.StandardError.ReadToEnd();
-        proc.WaitForExit();
-        EditorUtility.ClearProgressBar ();
-        if (proc.ExitCode != 0) {
-            Debug.LogError ("APK build error: " + err + "   code: " + proc.ExitCode);
-            throw new System.Exception ("APK 构建失败");
-        } else {
-            var files = Directory.GetFiles(path+"/build/outputs/apk/", "*unaligned.apk", System.IO.SearchOption.AllDirectories);
-            foreach (var f in files) 
-            {
-                Debug.LogWarning("remove unaligned file :"+f);
-                File.Delete(f);
-            }
-            if(!string.IsNullOrEmpty(finishAPKPath)){
-                var apkFiles = Directory.GetFiles(path+"/build/outputs/apk/", "*.apk", System.IO.SearchOption.AllDirectories);
-                if(apkFiles.Length>0){
-                    File.Move(apkFiles[0],finishAPKPath);
-                }else{
-                    Debug.LogError("no apk file found.");   
-                }
-            }
-            Debug.Log ("APK 构建成功.");
-            Debug.Log ("==============================================================");
-            if(isOpenFolderOnFinish)
-                OpenInFileBrowser.Open(path+"/build/outputs/apk/");
-        }
-    }
 }
